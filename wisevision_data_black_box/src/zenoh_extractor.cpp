1aeeb4be227a4cdb823880bc3eecbe6d6fdafaee
Khasreto <adam-krawczyk@outlook.com>
2025-02-16 19:55:36 +0100

    Stabilization/2501 (#4)
    * Update dockerfile and add  ci for ros2 tests (#2)
    
    * Add dockerfile and update ci for ros2 tests
    
    * Added sync to dev
    
    * Added workspace clean up to ci
    
    * Add license (#3)
    
    * Add license
    
    * Update check license workflow
    
    * Remove old license line
    
    * Updated to wisevision runner (#5)
    
    ---------
    
    Co-authored-by: czarekk215 <128484058+czarekk215@users.noreply.github.com>
    Co-authored-by: Bartlomiej Styczen <styczen.bartlomiej@gmail.com>

--- wisevision_data_black_box/src/zenoh_extractor.cpp
+++ wisevision_data_black_box/src/zenoh_extractor.cpp
@@ -1,4 +1,13 @@
-// Copyright (c) 2024, WiseVision. All rights reserved.
+/*
+ * Copyright (C) 2025 wisevision
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "zenoh_extractor.hpp"
 
 DataBaseHandler::DataBaseHandler(const std::string& storage_type, const std::string& url)
